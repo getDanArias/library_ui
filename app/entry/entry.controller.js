b0c3ff90cff4ac29144e5662129cfc6b8bf783a6
DecaHub <dan@decahub.com>
2016-11-13 10:25:24 -0500

    Add asset entry form

--- app/entry/entry.controller.js
+++ app/entry/entry.controller.js
@@ -4,7 +4,11 @@ angular.module("app").controller("EntryController", ($scope, Asset) => {
 	
 	console.log("EntryController");
 	
-	$scope.asset = {
+	const SINGLE_INC = 1;
+	
+	$scope.asset = new Asset();
+	
+	$scope.asset.data = {
 		author: "",
 		category: "",
 		media: "",
@@ -12,10 +16,63 @@ angular.module("app").controller("EntryController", ($scope, Asset) => {
 		title: "",
 		type: ""
 	};
-
+	
+	const postgresStringifyListProperty = (prop) => {
+		
+		const array = prop.toString().split(/\s*,\s*/);
+		const OFFSET = 1;
+		const LAST_ELEMENT = array.length - OFFSET;
+		const FIRST_ELEMENT = 0;
+		const ONE_ELEMENT = 1;
+		
+		let postgresStringArray = "";
+		
+		for (let element = FIRST_ELEMENT;
+				element < array.length; element += SINGLE_INC) {
+			
+			if (array.length === ONE_ELEMENT) {
+				
+				// There is only one element in the array
+				
+				postgresStringArray += `{"${array[element]}"}`;
+				
+			} else if (element === FIRST_ELEMENT) {
+				
+				// We are looking at the first element
+				
+				postgresStringArray += `{"${array[element]}", `;
+				
+			} else if (element === LAST_ELEMENT) {
+				
+				// We are looking at the last element
+				
+				postgresStringArray += `"${array[element]}"}`;
+				
+			} else {
+				
+				// We are looking at an in-between element
+				
+				postgresStringArray += `"${array[element]}", `;
+				
+			}
+			
+		}
+		
+		return postgresStringArray;
+	
+	};
+	
 	$scope.submitAsset = () => {
 		
-		console.log($scope.asset);
+		$scope.asset.data.author =
+			postgresStringifyListProperty($scope.asset.data.author);
+		$scope.asset.data.category =
+			postgresStringifyListProperty($scope.asset.data.category);
+
+		Asset.save($scope.asset.data, () => {
+			// Promise logic
+		});
+
 		
 	};
 	
