b610bfa605a8cc1519a83402d445a5bb11a821fd
DecaHub <dan@decahub.com>
2016-11-19 22:52:24 -0500

    Clean up database service

--- app/services/database.service.js
+++ app/services/database.service.js
@@ -1,18 +1,14 @@
 "use strict";
 
+/* eslint-disable no-invalid-this, consistent-this, object-shorthand,
+ no-ternary, func-names, sort-keys, no-prototype-builtins */
+
 angular.module("app").service("AssetDBService", function (Asset) {
 	
-	this.getAsset = (id) => {
-		
-		return Asset.get({id: id});
-		
-	};
 	
-	this.getAssets = () => {
-		
-		return Asset.query();
-		
-	};
+	this.getAsset = (id) => Asset.get({id: id});
+	
+	this.getAssets = () => Asset.query();
 	
 	this.createAsset = (asset) => {
 		
@@ -20,6 +16,19 @@ angular.module("app").service("AssetDBService", function (Asset) {
 		this.asset = asset;
 		
 		Asset.save(this.asset);
+		
+	};
+	
+	this.changeAsset = (id, changedAsset) => {
+		
+		const asset = Asset.get({id: id}, () => {
+			
+			console.log(asset);
+			console.log(asset.data);
+			
+			Asset.update({id: asset.data.id}, changedAsset);
+			
+		});
 		
 	};
 	
