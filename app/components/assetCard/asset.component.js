9f20aebfaabf26bfae70c6a765f2078b428f9472
DecaHub <dan@decahub.com>
2016-11-19 22:47:29 -0500

    Improve assetCard component by using ui bootstrap modal

--- app/components/assetCard/asset.component.js
+++ app/components/assetCard/asset.component.js
@@ -1,7 +1,15 @@
 "use strict";
 
+/* eslint-disable no-invalid-this, consistent-this, object-shorthand,
+ no-ternary, func-names, sort-keys, no-prototype-builtins */
+
 angular.module("app").component("assetCard", {
-	bindings: {asset: "="},
+	
+	bindings: {
+		asset: "=",
+		open: "&"
+	},
 	controller: "AssetComponentController",
 	templateUrl: "/components/assetCard/asset.component.html"
+	
 });
